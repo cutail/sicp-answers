dbf154a48b341f396962634c976ff58d8e143f7c
cutail <cutail.capricorn@gmail.com>
2016-07-05 15:28:56 +0800

    Update ex1.3.rkt

--- ex1.3.rkt
+++ ex1.3.rkt
@@ -1,6 +1,10 @@
+
+;; calculate the square of x
 (define (square x) (* x x))
+;; calculate the square sum of two parameters
 (define (square-sum x y) (+ (square x) (square y)))
 
+;; find the bigger two elements from a parameter list
 (define (bigger-two x y . slice)
   (cond ((null? slice) (list x y))
         (else (if (< x y) (cond ((> (car slice) x) (apply bigger-two y (car slice) (cdr slice)))
